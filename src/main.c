f8c94426aee00f7fad754dca67f97fd5ba1715d9
azokal <azokal@localhost>
2011-03-08 12:00:29 +0100

    main hg !

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for 42sh in /home/consta_m/prog/42sh
+** 
+** Made by maxime constantinian
+** Login   <consta_m@epitech.net>
+** 
+** Started on  Tue Mar  8 11:45:43 2011 maxime constantinian
+** Last update Tue Mar  8 11:46:38 2011 maxime constantinian
+*/
+
+#include	<stdio.h>
+
+int		main()
+{
+  printf("Main :DDDDD\n");
+  return (0);
+}
