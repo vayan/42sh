f0fca8699b3a1b609156a8e49c8bde2ad68b7f28
Yann Vaillant <yann@vaillant.im>
2011-03-08 11:25:50 +0100

    a la norme

--- src/main.c
+++ src/main.c
@@ -5,13 +5,13 @@
 ** Login   <consta_m@epitech.net>
 ** 
 ** Started on  Tue Mar  8 11:45:43 2011 maxime constantinian
-** Last update Tue Mar  8 11:46:38 2011 maxime constantinian
+** Last update Tue Mar  8 11:25:39 2011 yann vaillant
 */
 
 #include	<stdio.h>
 
 int		main()
 {
-  printf("Main :DDDDD\n");
+  printf("Main vive yann\n");
   return (0);
 }
