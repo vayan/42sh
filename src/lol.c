5a92611aa2f7adbed4c91dbd0be09aa5899b8820
Yann Vaillant <yann@vaillant.im>
2011-03-08 11:23:48 +0100

    rm vivemoi

--- src/lol.c
+++
@@ -1,9 +0,0 @@
-/*
-** lol.c for  in /home/vailla_y//Projet/42sh/src
-** 
-** Made by yann vaillant
-** Login   <vailla_y@epitech.net>
-** 
-** Started on  Tue Mar  8 11:21:28 2011 yann vaillant
-** Last update Tue Mar  8 11:21:29 2011 yann vaillant
-*/
