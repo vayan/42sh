511df5eaa59edcaa3b460f743e0286056a597b7b
Maxime Constantinian <azokal@users.noreply.github.com>
2011-04-27 00:01:02 +0200

    maj .h

--- include/graph.h
+++ include/graph.h
@@ -1,3 +1,13 @@
+/*
+** graph.h for 42sh in /home/consta_m/prog/42sh
+** 
+** Made by maxime constantinian
+** Login   <consta_m@epitech.net>
+** 
+** Started on  Tue Apr 26 19:34:35 2011 maxime constantinian
+** Last update Tue Apr 26 19:34:37 2011 maxime constantinian
+*/
+
 #ifndef		GRAPH_H__
 # define	GRAPH_H__
 
