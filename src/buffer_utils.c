3349202293f9d5b736cd15e22b79a3acb2df90cf
Nevermethion <Nevermethion@localhost>
2011-05-10 20:27:10 +0200

    cc failed

--- src/buffer_utils.c
+++ src/buffer_utils.c
@@ -5,25 +5,26 @@
 ** Login   <maurin_t@epitech.net>
 ** 
 ** Started on  Wed May  4 19:59:04 2011 timothee maurin
-** Last update Tue May 10 14:43:10 2011 timothee maurin
+** Last update Tue May 10 20:26:55 2011 timothee maurin
 */
 
-#include<unistd.h>
-#include<stdio.h>
-#include<sys/ioctl.h>
-#include<sys/types.h>
-#include<termcap.h>
-#include<termios.h>
-#include<stdlib.h>
-#include<curses.h>
-#include<term.h>
-#include<signal.h>
-#include<string.h>
-#include"shell.h"
-#include"xmalloc.h"
-#include<errno.h>
-#include<string.h>
-intverif_touche(char *cha)
+#include	<unistd.h>
+#include	<stdio.h>
+#include	<sys/ioctl.h>
+#include	<sys/types.h>
+#include	<termcap.h>
+#include	<termios.h>
+#include	<stdlib.h>
+#include	<curses.h>
+#include	<term.h>
+#include	<signal.h>
+#include	<string.h>
+#include	"shell.h"
+#include	"xmalloc.h"
+#include	<errno.h>
+#include	<string.h>
+
+int		verif_touche(char *cha)
 {
   if (cha[0] < ' ' || cha[0] == 127)
     return (1);
@@ -31,7 +32,7 @@ intverif_touche(char *cha)
     return (0);
 }
 
-intis_del(char *cha)
+int	is_del(char *cha)
 {
   if (cha[0] == 27 && cha[1] == 91 && cha[2] == 51 && cha[3] == 126)
     return (1);
@@ -39,7 +40,7 @@ intis_del(char *cha)
     return (0);
 }
 
-intnbr_column()
+int	nbr_column()
 {
   struct winsize        w;
 
