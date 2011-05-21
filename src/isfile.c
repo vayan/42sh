e023185672e8534fc8c994547c20d35b0419bb6a
Maxime Constantinian <azokal@users.noreply.github.com>
2011-05-21 15:43:13 +0200

    modif tab de hach

--- src/isfile.c
+++ src/isfile.c
@@ -5,7 +5,7 @@
 ** Login   <consta_m@epitech.net>
 ** 
 ** Started on  Sun Apr 10 17:51:08 2011 maxime constantinian
-** Last update Wed May  4 03:07:02 2011 maxime constantinian
+** Last update Sat May 21 15:42:11 2011 maxime constantinian
 */
 
 #include	<sys/stat.h>
@@ -20,7 +20,7 @@ int		isfile(char *str)
       fprintf(stderr, "42sh: lstat failded\n");
       return (0);
     }
-  if (S_ISREG(buf.st_mode))
+  if (S_ISREG(buf.st_mode) || S_ISLNK(buf.st_mode))
     return (1);
   else
     return (0);
