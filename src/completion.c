36670b8b4ac9768c31181f29b13a7667aea47eca
Timothee Maurin <Nevermethion@users.noreply.github.com>
2011-05-21 17:25:15 +0200

    diverse correction de bug

--- src/completion.c
+++ src/completion.c
@@ -5,7 +5,7 @@
 ** Login   <maurin_t@epitech.net>
 ** 
 ** Started on  Thu May 12 13:09:38 2011 timothee maurin
-** Last update Thu May 19 18:26:43 2011 timothee maurin
+** Last update Sat May 21 16:52:10 2011 timothee maurin
 */
 
 #include <string.h>
@@ -21,21 +21,16 @@
 
 void		add_all_other(t_completion **completion, t_hach_bin *hach)
 {
-  int		test;
   int		i;
 
   i = 0;
-  test = 0;
   while (i < 94)
     {
       if (hach && hach->next && hach->next[i] != 0)
-	{
-	  add_all_other(completion, hach->next[i]);
-	  test = 1;
-	}
+	add_all_other(completion, hach->next[i]);
       i++;
     }
-  if (test == 0)
+  if (hach->path)
     {
       i = strlen(hach->path);
       while (i > 0 && hach->path[i] != '/')
@@ -49,11 +44,12 @@ t_completion	*find_commande(char *buf, int pos, int begin, t_hach_bin *hach)
   t_completion	*completion;
 
   completion = 0;
-  while ((begin - 1) < pos && begin != -1)
+  while ((begin) < pos && begin != -1)
     {
-      if (hach->next && hach->next[buf[begin - 1] - 32])
-	hach = hach->next[buf[begin - 1] - 32];
-      else
+      if (hach->next && buf[begin] > 32
+	  && hach->next[buf[begin] - 32])
+	hach = hach->next[buf[begin] - 32];
+      else if (buf[begin] > 32)
 	begin = -2;
       begin++;
     }
@@ -76,10 +72,17 @@ t_completion	*find_in_folder(char *buf, int pos, int begin)
     dirp = xopendir(".");
   else
     {
-      tmp = xmalloc((pos - tmp2 + 2) * sizeof(*tmp));
-      strncpy(tmp, &(buf[begin]), tmp2 - begin + 1);
-      tmp[tmp2 - begin + 1] = '\0';
-      dirp = xopendir(tmp);
+      while(buf[begin] == 9)
+	begin++;
+      if (begin < tmp2)
+	{
+	  tmp = xmalloc((tmp2 - begin + 2) * sizeof(*tmp));
+	  strncpy(tmp, &(buf[begin]), tmp2 - begin + 1);
+	  tmp[tmp2 - begin + 1] = '\0';
+	  dirp = xopendir(tmp);
+	}
+      else
+	dirp = xopendir(".");	
       begin =  tmp2 + 1;
     }
   if (dirp != 0)
@@ -97,7 +100,7 @@ t_completion	*find_completion(int type, char *buf, int pos, int begin)
 
   tmp = recup_shell(0);
   if (type)
-    return (find_commande(buf, pos, begin, tmp->tab_hach));
+    return (find_commande(buf, pos, begin - 1, tmp->tab_hach));
   else
     return (find_in_folder(buf, pos, begin));
 }
@@ -109,7 +112,7 @@ void		traiter_list(t_completion *compl, char *buf, int *i, int *pos)
   if ((be = *pos || 1) && *pos != 0 || buf[*pos] != ';')
     while (be > 0 && buf[be] !=  ' ' && buf[be] != 9 && buf[be] != ';')
       be--;
-  while (buf[be - 1] == '/')
+  while (be > 0 && buf[be - 1] == '/')
     be--;
   if (compl)
     {
