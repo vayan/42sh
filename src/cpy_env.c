ca1c6570a982b080de4164e2cb45ce100794e61f
Timothee Maurin <Nevermethion@users.noreply.github.com>
2011-06-09 20:00:40 +0200

    ca ne se suspend plus->gerer a coup de signal (voir main), reste a mettre a la norme

--- src/cpy_env.c
+++ src/cpy_env.c
@@ -5,7 +5,7 @@
 ** Login   <vailla_y@epitech.net>
 ** 
 ** Started on  Tue Apr 26 14:59:40 2011 yann vaillant
-** Last update Wed May  4 17:28:31 2011 maxime constantinian
+** Last update Thu Jun  9 19:58:48 2011 timothee maurin
 */
 
 #include	<stdlib.h>
@@ -22,11 +22,12 @@ int		get_nbr_ligne_tab(char **env)
   return (x);
 }
 
-void	copy_env(char **env, t_shell *shell)
+int	copy_env(char **env, t_shell *shell)
 {
   int	x = get_nbr_ligne_tab(env);
 
   shell->env = xmalloc((x + 1) * sizeof(*(shell->env)));
   while (x--)
     shell->env[x] = my_strcopynalloc(env[x]);
+  return (0);
 }
